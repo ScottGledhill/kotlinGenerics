3ad12e9f202d1aaef15643e8140497c3bbe83d9c
ScottGledhill <scottgledhill@hotmail.co.uk>
2020-11-13 19:37:43 +0000

    added contest class

--- src/Contest.kt
+++ src/Contest.kt
@@ -1,2 +1,15 @@
-class Contest {
+class Contest<T: Pet> {
+    val scores: MutableMap<T, Int> = mutableMapOf() // T is generic type of Pet that the contest is dealing with
+
+    fun addScore(t: T, score:Int = 0) {
+        if (score >= 0) scores.put(t, score)
+    }
+    fun getWinners() : MutableSet<T> {
+        val highScore = scores.values.maxOrNull()
+        val winners: MutableSet<T> = mutableSetOf()
+        for ((t, score) in scores) {
+            if (score == highScore) winners.add(t)
+        }
+        return winners
+    }
 }
